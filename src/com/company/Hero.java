8c512293e03b586e6527649e492b65496b00d85d
albertik004 <albert.nurlanovv@mail.ru>
2021-11-09 02:25:36 +0600

    Исправил недочет.1

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -4,11 +4,24 @@ public class Hero {
     private int health;
     private int damage;
     private String megapower;
+    private String names;
 
 
-    public Hero(int health, int damage) {
+    public Hero( String names, int health, int damage) {
         this.health = health;
         this.damage = damage;
+        this.names = names;
+    }
+
+    public Hero( String names, int health) {
+        this.health = health;
+        this.names = names;
+    }
+    public Hero(String names, int health, int damage, String megapower) {
+        this.names = names;
+        this.health = health;
+        this.damage = damage;
+        this.megapower = megapower;
     }
 
     public int getHealth() {
@@ -24,9 +37,9 @@ public class Hero {
     }
 
 
-    public Hero(int health, int damage, String megapower) {
-        this.health = health;
-        this.damage = damage;
-        this.megapower = megapower;
+    public String getNames() {
+        return names;
     }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,22 @@ public class Main {
         boss.setHealth(1001);
         boss.setDamage(70);
         boss.setMegapower("Огниво !!!");
-        System.out.println("Megaladon " + boss.getHealth() + " \nUron " + boss.getDamage() + "\nMegapower " + boss.getMegapower());
+        System.out.println("Megaladon |" + boss.getHealth() + " \nUron |" + boss.getDamage() + "\nMegapower |" + boss.getMegapower());
+        createheroes();
+    }
 
+    public static Hero[] createheroes() {
+        System.out.println("-----------------------------------");
+        System.out.println("Comand Heroes:");
+        Hero alhimic = new Hero("Alhimic -- ", 200, 20, "Magic!");
+        Hero antimag = new Hero("Antimag -- ", 150, 25, "Teleport!");
+        Hero pudg = new Hero("Pudg -- ", 350, 30, "Kruk!");
+        Hero axe = new Hero("Axe -- ", 300, 35, "XTWO Damage!");
+        Hero[] teamspirit = {alhimic, antimag, pudg, axe};
+        for (int i = 0; i < teamspirit.length; i++) {
+            System.out.println(teamspirit[i].getNames() + " HP:" + teamspirit[i].getHealth() + " | URON:" + teamspirit[i].getDamage() + " | MegaPower:" + teamspirit[i].getMegapower());
+
+        }
+        return teamspirit;
     }
 }
