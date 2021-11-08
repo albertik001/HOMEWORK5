1ba5adbdeed5288e1547f97437164e8f7f3bb6fa
albertik004 <albert.nurlanovv@mail.ru>
2021-11-08 22:55:49 +0600

    Исправил недочет.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Boss Boss = new Boss();
-        Boss.setHealth(1001);
-        Boss.setDamage(70);
-        Boss.setMegapower("Огниво !!!");
-        System.out.println("Megaladon " + Boss.getHealth() + " \nUron " + Boss.getDamage() + "\nMegapower " + Boss.getMegapower());
+        Boss boss = new Boss();
+        boss.setHealth(1001);
+        boss.setDamage(70);
+        boss.setMegapower("Огниво !!!");
+        System.out.println("Megaladon " + boss.getHealth() + " \nUron " + boss.getDamage() + "\nMegapower " + boss.getMegapower());
 
     }
 }
